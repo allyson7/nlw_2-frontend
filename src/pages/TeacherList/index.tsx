feb09a38db794284244c27d635a80cf4acf6118f
Allysonlopes <allyson@m2smart.com.br>
2021-01-23 12:06:25 -0400

    fifth Commit

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader />
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="subject">Dia da semana</label>
+            <input type="text" id="week_day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="subject">Hora</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars.githubusercontent.com/u/31413976?s=460&u=1032e1a56aa3e043562f4959a67f5aded6d37ff0&v=4" alt="Allyson Lopes"/>
+            <div>
+              <strong>Allyson Lopes</strong>
+              <span>Química</span>
+            </div>
+          </header>
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br/><br/>
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 20,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
